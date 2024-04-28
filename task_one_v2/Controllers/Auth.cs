3551addac15ef0213e8e36cca09bbe3aa11d56d9
abdullahkhrisstoo <abd.abdullah2000@gmail.com>
2024-04-28 18:21:47 +0300

    Add project files.

---
+++ task_one_v2/Controllers/Auth.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace task_one_v2.Controllers
+{
+    public class Auth : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+        public IActionResult PrivicyPolicy()
+        {
+            return View();
+        }
+        public IActionResult TermsAndCondition()
+        {
+            return View();
+        }
+    }
+}
